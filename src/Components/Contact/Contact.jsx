26926bde1fb01c348dc885959413434b715c4902
TUrielRios <riostiziano6@gmail.com>
2023-12-19 13:02:06 -0300

    linkedinggithub

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,12 +12,12 @@ const Contact = () => {
         <p>Or give me a call or message at: +54 011 6889-0924</p>
         <p>Feel free to check out my <a href="https://www.flipsnack.com/5AA7B699E8C/cv-tiziano-rios-full-stack-developer/full-view.html" target="_blank" rel="noopener noreferrer">CV</a>.</p>
                 {/* GitHub Icon and Link */}
-                <a href="https://github.com/YourGitHubUsername" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/TUrielRios" target="_blank" rel="noopener noreferrer">
           <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" className={styles.icon} />
         </a>
 
         {/* LinkedIn Icon and Link */}
-        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/tiziano-rios-049b5b264/" target="_blank" rel="noopener noreferrer">
           <img src="https://cdn-icons-png.flaticon.com/256/174/174857.png" alt="LinkedIn" className={styles.icon} />
         </a>
     </section>
