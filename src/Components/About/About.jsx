7eb02cbde4b719f8ad0da12b6f6744133aa9e68f
TUrielRios <riostiziano6@gmail.com>
2023-12-19 23:59:40 -0300

    responsive

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,11 +8,11 @@ const About = () => {
       <div className={styles.aboutContent}>
         <h1>Hi, I'm Tiziano</h1>
         <p>
-          I'm a web developer specializing in PERN and MERN stack. My skills include HTML5, CSS3, JavaScript, TypeScript, React, Node, Sequelize,PostgreSQL,MongoDB,Next.js,Redux Toolkit,Express and more.
-          I believe in creating clean, efficient, and user-friendly websites that leave a lasting impression.
+        I am a proficient full-stack web developer based in Buenos Aires, Argentina, with expertise in both PERN and MERN stacks. <br /><br /> My skill set includes HTML5, CSS3, JavaScript, TypeScript, React, Node, Sequelize, PostgreSQL, MongoDB, Next.js, Redux Toolkit, Express, and a variety of other cutting-edge technologies.
+        My philosophy revolves around creating elegant and high-performance websites that prioritize clean code and user-centric design. I am dedicated to leaving a lasting impression by delivering seamless and memorable online experiences
         </p>
         <p>
-          Outside of coding, I enjoy exploring new technologies, listening to music, and collaborating on exciting projects.
+          In my free time, I enjoy exploring new technologies, listening to music, and playing football.
         </p>
       </div>
     </section>
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,10 +6,10 @@ const Contact = () => {
   return (
     <section id="contact" className={styles.contact}>
         <h2>Contact me!</h2>
-        <p>I'm excited to hear about your upcoming project! Let's talk about how I can help you.</p>
+        <p>I'm excited to hear about your upcoming project! Let's talk about how can I help you.</p>
         {/* Add your contact form or contact information here */}
         <p>You can reach me via: <a href="mailto:riostiziano6@gmail.com">riostiziano6@gmail.com</a></p>
-        <p>Or give me a call or message at: +54 011 6889-0924</p>
+        <p>Or give me a call or a message at: +54 011 6889-0924</p>
         <p>Feel free to check out my <a href="https://www.flipsnack.com/5AA7B699E8C/cv-tiziano-rios-full-stack-developer/full-view.html" target="_blank" rel="noopener noreferrer">CV</a>.</p>
                 {/* GitHub Icon and Link */}
                 <a href="https://github.com/TUrielRios" target="_blank" rel="noopener noreferrer">
