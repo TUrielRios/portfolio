12bc43a592bf67e6dac78c9247b2f9cbf51668c0
TUrielRios <riostiziano6@gmail.com>
2023-12-20 00:00:58 -0300

    respo

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className={styles.aboutContent}>
         <h1>Hi, I'm Tiziano</h1>
         <p>
-        I am a proficient full-stack web developer based in Buenos Aires, Argentina, with expertise in both PERN and MERN stacks. <br /><br /> My skill set includes HTML5, CSS3, JavaScript, TypeScript, React, Node, Sequelize, PostgreSQL, MongoDB, Next.js, Redux Toolkit, Express, and a variety of other cutting-edge technologies.
+        I am a proficient full-stack web developer from Buenos Aires, Argentina, with expertise in both PERN and MERN stacks. <br /><br /> My skill set includes HTML5, CSS3, JavaScript, TypeScript, React, Node, Sequelize, PostgreSQL, MongoDB, Next.js, Redux Toolkit, Express, and a variety of other cutting-edge technologies.
         My philosophy revolves around creating elegant and high-performance websites that prioritize clean code and user-centric design. I am dedicated to leaving a lasting impression by delivering seamless and memorable online experiences
         </p>
         <p>
